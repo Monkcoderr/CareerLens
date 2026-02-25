a220f0b052437584ba6de89590aa377a49ef02bb
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2026-02-25 23:34:45 +0530

    fix: comment out updateProfile route in authRoutes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,6 +6,6 @@ const { protect } = require('../middleware/auth');
 router.post('/register', register);
 router.post('/login', login);
 router.get('/profile', protect, getProfile);
-router.put('/profile', protect, updateProfile);
+// router.put('/profile', protect, updateProfile);
 
 module.exports = router;
