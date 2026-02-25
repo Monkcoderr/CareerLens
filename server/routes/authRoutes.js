894ea4cd46f39ea808eeef71d34bebdccad39f15
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2026-02-25 23:35:25 +0530

    fix: uncomment getProfile and updateProfile routes in authRoutes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,7 +5,7 @@ const { protect } = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
-// router.get('/profile', protect, getProfile);
-// router.put('/profile', protect, updateProfile);
+router.get('/profile', protect, getProfile);
+router.put('/profile', protect, updateProfile);
 
 module.exports = router;
